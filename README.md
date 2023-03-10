00dc5889cd28c7fdad4fbf9a3753ae5241141a3d
Scott Rouse <scott.rouse@gmail.com>
2023-03-09 20:39:41 -0600

    adding content

--- README.md
+++ README.md
@@ -1,85 +1,75 @@
-# Summit Tokenizer
+# Portfolio Examples
 <!-- 
   Do not edit directly, built using contentful-readme-generator.
   Content details in Build Information below.
 -->
 
-- [Description](#description)
-- [How To Install](#how-to-install)
+- [Third Space](#third-space)
+- [Aha Notes](#aha-notes)
+- [American Family Insurance Jutro Customization](#american-family-insurance-jutro-customization)
+- [Summit Next Generation UX](#summit-next-generation-ux)
+- [Summit Loan Member Visualization](#summit-loan-member-visualization)
+- [Summit Content Platform](#summit-content-platform)
 - [Build Information](#build-information)
 
 ---
 
 
-__Title__: Summit Tokenizer
+__Title__: Scott Rouse Examples
 
-<span class="badge-npmversion"><a href="https://npmjs.org/package/scu-tokenizer" title="View this project on NPM"><img src="https://img.shields.io/npm/v/scu-tokenizer.svg" alt="NPM version" /></a></span>
-<span class="badge-npmdownloads"><a href="https://npmjs.org/package/scu-tokenizer" title="View this project on NPM"><img src="https://img.shields.io/npm/dm/scu-tokenizer.svg" alt="NPM downloads" /></a></span>
+__Developer Emails__: scott.rouse@gmail.com
 
-__Project Abbreviation__: scu-tokenizer
+## Third Space
+Third Space was a start-up focused on creating communication and mentoring opportunities for employees within businesses. I helped research and visualize the core ideas driving the application and business model and then design out a first iteration.
 
-__Developer Emails__: scott.rouse@summitcreditunion.com
+[Third Space Visualizations](https://assets.ctfassets.net/rtkhko6y3s3u/5Ikc3nn6ALZMAzrXoFn8t3/4a63547b4fd3dd4dbbf4598a2c76e871/3S-UX-v00_00_01.pdf)
 
-__NPM URL__: https://www.npmjs.com/package/scu-tokenizer
 
-__Repo URL__: https://bitbucket.org/rouse_scu/scu-tokenizer/admin
+[Third Space Designs](https://assets.ctfassets.net/rtkhko6y3s3u/5lYLZCmH7FgWe42cdGclN3/8064e543c34cef74c5a4123c5d3a2023/3S-Designs-v00_01.pdf)
 
-## Description
 
-scu-tokenizer is a tokenizer using a Figma file as the source 
-of truth for all tokens with an eye towards enabling more powerful token transformations.
+## Aha Notes
+Aha Notes, a notebook and note taking app, was a personal project that I built with a team including an engineer, project manager, and marketing friends. It featured a patent pending physical notebook bound with a magnetic binding and a phone app that displayed a meaningful snippet of the note image as the list title. 
 
-## How To Install
+[Aha Designs 2019-04](https://assets.ctfassets.net/rtkhko6y3s3u/9CySGbbUM6FCeKcERrvWj/f46c5deb968fd1f47b300193a491989d/2019-04-08-AHA-designs.pdf)
 
-Config file example:
-```
-var builds = require('scu-tokenizer/dist/index.js');// BUILD FROM NPM PACKAGE
-require('dotenv').config();
-const paths = require('../paths');
-const log = require('../builds/utils/building/logging').createLogger('Tokens');
-const globalCss = require('../cssComponents/cssComps/global.scss.cjs.js');
 
-async function buildTokens() {
-  const distPath = paths.getDist();
-  return builds.buildNode({
-    figmaFileIDs: process.env.DSYS_COMPONENTS_ENGINE_FIGMA_FILE_ID,
-    figmaToken: process.env.DSYS_COMPONENTS_ENGINE_FIGMA_ACCESS_TOKEN,
-    buildPath: `${distPath}/scu/reference/tokens/`,
-    fontConfigs: [
-      {
-        path: __dirname + "/fonts/Proxima/proximanova_semibold_macroman/proximanova-semibold-webfont.woff",
-        name: "Proxima Nova",
-        weight: 600,
-        style: "normal",
-      }
-    ],
-    prefix: 'scu',
-    hooks: {
-      preCssHook: () => {
-        return globalCss.css();
-      }
-    }
-  }).then(() => {
-    log.end();
-    return true;
-  });
-}
+## American Family Insurance Jutro Customization
+American Family, a fortune 300 company, was migrating to a single, centralized insurance API across multiple brands without also systematizing the UI. Utilizing and manipulating their own graphics, I was able to illustrate to upper management the risks of not developing a design system for the UI.
 
-exports.buildTokens = buildTokens;
-```
+[AmFam E1P Jutro Customization](https://assets.ctfassets.net/rtkhko6y3s3u/13bn1LpXJfE4IVCzXIMvv0/47aeb86c242caa430c66cfc979a7dd4e/e1p-JutroCustomization--v00-08.pdf)
+
+
+## Summit Next Generation UX
+This was done during my time in Summit's Design and Innovation Team. It was an exploration of what our various UI form factors could look like if we designed into the major use cases. It includes a (speculative) usage map informing mobile, desktop, and in-branch experiences.
+
+[Summit Universal UX 2019-03](https://assets.ctfassets.net/rtkhko6y3s3u/1crcQdf3htA7XlGf6oEwx7/9c309acca8c09e8a220fffe1c5f70f67/2019-03-00-SCU-UUX.pdf)
+
+
+## Summit Loan Member Visualization
+Another Summit Design and Innovation project, LOUI (Loan Object UI) was an attempt to standardize the expression of a loan within Summit in an effort to visualize that loan across various contexts of the member's experience. This particular visualization was designed to augment an in-person conversation between a Loan Officer and a member.
+
+[Summit LOUI 2019-03](https://assets.ctfassets.net/rtkhko6y3s3u/dLy1F3ewhq2P9ckeAuAon/4790fa74b9beb564559f61a4c5e2c5ef/2019-03-26-Summit-LOUI.pdf)
+
+
+## Summit Content Platform
+Summit managed their website with Drupal 7 which was nearing end of life in a year or so. I was asked to research alternatives and presented a content platform, Contentful, as a solution to break out of the silo of CMS managed websites. Summit signed on and now is rebuilding their website as well as centralizing rates, branch locations, and all product information in Contentful. That information is now accessible from any and all parts of Summit's offerings. The Contentful contact described it as one of the most impressive presentations he had seen.
+
+[Summit Content Platform Presentation](https://assets.ctfassets.net/rtkhko6y3s3u/6zzka6j6V8U6bgnuQ0aTo7/a1dfb43325c67c9e22379fa688e64196/scu-content-platform-2022-06-20.pdf)
+
 
 ## Build Information
 
 *Dynamically built using contentful-readme-generator. Do not edit directly.*
 
-*__updated__: 3/9/2023, 10:47:56 AM*
+*__updated__: 3/9/2023, 8:36:36 PM*
 
-*__built__: 3/9/2023, 3:19:11 PM*
+*__built__: 3/9/2023, 8:37:15 PM*
 
-*__space__: 7gg213tt004u*
+*__space__: rtkhko6y3s3u*
 
-*__environment__: sandbox-readme*
+*__environment__: master*
 
-*__entity id__: 57porcJFEFtM8Ez24WKbQT*
+*__entity id__: 5C19mtrlFY7JSeMVoXEoIl*
 
-[Edit Contentful Entry](https://app.contentful.com/spaces/7gg213tt004u/environments/sandbox-readme/entries/57porcJFEFtM8Ez24WKbQT)
+[Edit Contentful Entry](https://app.contentful.com/spaces/rtkhko6y3s3u/environments/master/entries/5C19mtrlFY7JSeMVoXEoIl)
--- templates/htmlTemplate.mjs
+++ templates/htmlTemplate.mjs
@@ -5,11 +5,211 @@ export default function renderHTML(content, css) {
 <html>
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="https://readme-design-system.netlify.app/transformations/fds-web/rds.css" />
+    <script src="https://readme-design-system.netlify.app/transformations/fds-web/icons/dam-widget-web-comp.js"></script>
     <style>
       ${css}
     </style>
+    <style>
+
+      html, body {
+        font-size: 16px;
+        margin: 0; padding: 0;
+        background-color: var( --rds-color-primary-5 );
+        color: var( --rds-color-grey-2 );
+      }
+
+      .markdown-body {
+        font: var( --rds-type-text );
+      }
+
+      .markdown-body h2 {
+        font: var( --rds-type-text-8 );
+        margin-top: calc( var( --rds-spacing-2 ) + var( --rds-spacing-0-6 ));
+        margin-bottom: var( --rds-spacing-1 );
+        padding: 0;
+      }
+
+      .header {
+        background-color: var( --color-header );
+        color: var( --color-header-text );
+        font-size: 1.4em;
+        height: 140px;
+        display: flex;
+        justify-content: center;
+      }
+
+        .header-content {
+          display: flex;
+          justify-content: start;
+          align-items: center;
+          width: 100%;
+        }
+
+        .markdown-content,
+        .header-content {
+          padding-left: 80px;
+          padding-right: 80px;
+          max-width: 1000px;
+          margin: 0 auto;
+          font-size: 16px;
+        }
+
+        .markdown-content {
+          padding-top: 40px;
+          padding-bottom: 80px;
+        }
+
+        .markdown-content img {
+          box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+        }
+
+        .markdown-body hr {
+          height: 1px;
+        }
+
+      @media (max-width:768px) {
+        .markdown-content,
+        .header-content {
+          padding-left: 40px;
+          padding-right: 40px;
+          font-size: 14px;
+        }
+        .markdown-content {
+          padding-top: 30px;
+          padding-bottom: 40px;
+        }
+      }
+      @media (max-width:500px) {
+        .markdown-content, .header-content {
+          font-size: 12px;
+        }
+        .markdown-content {
+          padding-top: 20px;
+          padding-bottom: 30px;
+        }
+      }
+
+
+      @media (prefers-color-scheme: dark) {
+        .markdown-body {
+          color-scheme: dark;
+          --color-prettylights-syntax-comment: #8b949e;
+          --color-prettylights-syntax-constant: #79c0ff;
+          --color-prettylights-syntax-entity: #d2a8ff;
+          --color-prettylights-syntax-storage-modifier-import: #c9d1d9;
+          --color-prettylights-syntax-entity-tag: #7ee787;
+          --color-prettylights-syntax-keyword: #ff7b72;
+          --color-prettylights-syntax-string: #a5d6ff;
+          --color-prettylights-syntax-variable: #ffa657;
+          --color-prettylights-syntax-brackethighlighter-unmatched: #f85149;
+          --color-prettylights-syntax-invalid-illegal-text: #f0f6fc;
+          --color-prettylights-syntax-invalid-illegal-bg: #8e1519;
+          --color-prettylights-syntax-carriage-return-text: #f0f6fc;
+          --color-prettylights-syntax-carriage-return-bg: #b62324;
+          --color-prettylights-syntax-string-regexp: #7ee787;
+          --color-prettylights-syntax-markup-list: #f2cc60;
+          --color-prettylights-syntax-markup-heading: #1f6feb;
+          --color-prettylights-syntax-markup-italic: #c9d1d9;
+          --color-prettylights-syntax-markup-bold: #c9d1d9;
+          --color-prettylights-syntax-markup-deleted-text: #ffdcd7;
+          --color-prettylights-syntax-markup-deleted-bg: #67060c;
+          --color-prettylights-syntax-markup-inserted-text: #aff5b4;
+          --color-prettylights-syntax-markup-inserted-bg: #033a16;
+          --color-prettylights-syntax-markup-changed-text: #ffdfb6;
+          --color-prettylights-syntax-markup-changed-bg: #5a1e02;
+          --color-prettylights-syntax-markup-ignored-text: #c9d1d9;
+          --color-prettylights-syntax-markup-ignored-bg: #1158c7;
+          --color-prettylights-syntax-meta-diff-range: #d2a8ff;
+          --color-prettylights-syntax-brackethighlighter-angle: #8b949e;
+          --color-prettylights-syntax-sublimelinter-gutter-mark: #484f58;
+          --color-prettylights-syntax-constant-other-reference-link: #a5d6ff;
+
+          --color-fg-default: var( --rds-color-grey-9 );
+          --color-fg-muted: var( --rds-color-grey-8 );
+          --color-fg-subtle: var( --rds-color-grey-7 );
+          --color-canvas-default: var( --rds-color-grey-0 );
+          --color-canvas-subtle: var( --rds-color-grey-1 );
+          --color-border-default: var( --rds-color-grey-2 );
+          --color-border-muted: var( --rds-color-grey-1 );
+          --color-neutral-muted: var( --rds-color-grey-5 );
+          --color-accent-fg: var( --rds-color-primary-5 );
+          --color-accent-emphasis: var( --rds-color-primary-5 );
+          --color-attention-subtle: var( --rds-color-primary-1 );
+          --color-danger-fg: var( --rds-color-primary-1 );
+
+          --color-header: var( --rds-color-primary-5 );
+          --color-header-text: var( --rds-color-grey-10 );
+        }
+      }
+      
+      @media (prefers-color-scheme: light) {
+        .markdown-body {
+          color-scheme: light;
+          --color-prettylights-syntax-comment: #6e7781;
+          --color-prettylights-syntax-constant: #0550ae;
+          --color-prettylights-syntax-entity: #8250df;
+          --color-prettylights-syntax-storage-modifier-import: #24292f;
+          --color-prettylights-syntax-entity-tag: #116329;
+          --color-prettylights-syntax-keyword: #cf222e;
+          --color-prettylights-syntax-string: #0a3069;
+          --color-prettylights-syntax-variable: #953800;
+          --color-prettylights-syntax-brackethighlighter-unmatched: #82071e;
+          --color-prettylights-syntax-invalid-illegal-text: #f6f8fa;
+          --color-prettylights-syntax-invalid-illegal-bg: #82071e;
+          --color-prettylights-syntax-carriage-return-text: #f6f8fa;
+          --color-prettylights-syntax-carriage-return-bg: #cf222e;
+          --color-prettylights-syntax-string-regexp: #116329;
+          --color-prettylights-syntax-markup-list: #3b2300;
+          --color-prettylights-syntax-markup-heading: #0550ae;
+          --color-prettylights-syntax-markup-italic: #24292f;
+          --color-prettylights-syntax-markup-bold: #24292f;
+          --color-prettylights-syntax-markup-deleted-text: #82071e;
+          --color-prettylights-syntax-markup-deleted-bg: #ffebe9;
+          --color-prettylights-syntax-markup-inserted-text: #116329;
+          --color-prettylights-syntax-markup-inserted-bg: #dafbe1;
+          --color-prettylights-syntax-markup-changed-text: #953800;
+          --color-prettylights-syntax-markup-changed-bg: #ffd8b5;
+          --color-prettylights-syntax-markup-ignored-text: #eaeef2;
+          --color-prettylights-syntax-markup-ignored-bg: #0550ae;
+          --color-prettylights-syntax-meta-diff-range: #8250df;
+          --color-prettylights-syntax-brackethighlighter-angle: #57606a;
+          --color-prettylights-syntax-sublimelinter-gutter-mark: #8c959f;
+          --color-prettylights-syntax-constant-other-reference-link: #0a3069;
+
+          --color-fg-default: var( --rds-color-grey-2 );
+          --color-fg-muted: var( --rds-color-grey-4 );
+          --color-fg-subtle: var( --rds-color-grey-3 );
+          --color-canvas-default: var( --rds-color-grey-10 );
+          --color-canvas-subtle: var( --rds-color-grey-95 );
+          --color-border-default: var( --rds-color-grey-9 );
+          --color-border-muted: var( --rds-color-grey-9 );
+          --color-neutral-muted: var( --rds-color-grey-5 );
+          --color-accent-fg: var( --rds-color-primary-5 );
+          --color-accent-emphasis: var( --rds-color-primary-5 );
+          --color-attention-subtle: var( --rds-color-primary-9 );
+          --color-danger-fg: var( --rds-color-primary-9 );
+
+          --color-header: var( --rds-color-primary-5 );
+          --color-header-text: var( --rds-color-grey-10 );
+        }
+      }
+    </style>
   </head>
   <body class="markdown-body">
+      <div class="header">
+        <div class="header-content">
+          <rds-icon-dam-widget
+            style="
+              width: 28px;
+              --color: var( --rds-color-grey-10 );
+              margin-right: var( --rds-spacing-1-6 )">
+          </rds-icon-dam-widget>
+          <div style="font: var( --rds-type-text-8 );">
+            Scott Rouse
+          </div>
+        </div>
+      </div>
       <div class="markdown-content">
         ${content}
       </div>
