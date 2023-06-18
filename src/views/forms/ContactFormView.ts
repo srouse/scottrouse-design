7feb574b172a4d013550513de3a28ab683f3eaa1
srouse <scott.rouse@gmail.com>
2023-06-17 19:43:17 -0500

    adding custom success page

--- src/views/forms/ContactFormView.ts
+++ src/views/forms/ContactFormView.ts
@@ -4,7 +4,10 @@ import { html } from "scu-ssg";
 
 export async function renderContactFormView() {
   return html`
-    <form name="contact" netlify>
+    <form
+      name="contact"
+      action="/?success"
+      netlify>
       <label>Name<input type="text" name="name" /></label>
       <button type="submit">Send</button>
     </form>
