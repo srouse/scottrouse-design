ef58f803c23c5d1eeded617301e5a8488a610fb9
Scott Rouse <scott.rouse@gmail.com>
2025-01-26 10:39:46 -0600

    getting all three up

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,12 @@
 export const SCOTT_ROUSE_OG_WEBSITE = '7CKULoxRapxruVVhstelFa';
 export const SCOTT_ROUSE_DEV_WEBSITE = '5yEhKGITpyUGlVxDYlwnKM';
+export const SCOTT_ROUSE_DESIGN_WEBSITE = 'HqCeYXMFu4GyL36tfgKwG';
+export const SCOTT_ROUSE_SYSTEM_WEBSITE = '54WtLpkgVfBGmH7jIBjoho';
 
-export const SCOTT_ROUSE_OG_TAG = 'websiteScottRouseOg';
-export const SCOTT_ROUSE_DEV_TAG = 'websiteScottRouseDev';
-export const SCOTT_ROUSE_DESIGN_TAG = 'websiteScottRouseDesign';
-export const SCOTT_ROUSE_DESIGN_SYSTEM_TAG = 'websiteScottRouseDesignSystem';
+export const SCOTT_ROUSE_OG_TAG = 'scottRouseOg';
+export const SCOTT_ROUSE_DEV_TAG = 'scottRouseDev';
+export const SCOTT_ROUSE_DESIGN_TAG = 'scottRouseDesign';
+export const SCOTT_ROUSE_DESIGN_SYSTEM_TAG = 'scottRouseDesignSystem';
 
 // This works client side as well...
 if (typeof process !== 'undefined') {
@@ -13,4 +15,4 @@ if (typeof process !== 'undefined') {
 
 // Preview needs these vars...
 export const PREVIEW_CONTENTFUL_SPACE = process.env.PUBLIC_CONTENTFUL_SPACE;
-export const PREVIEW_CONTENTFUL_ENVIRONMENT = process.env.PUBLIC_CONTENTFUL_SPACE;
+export const PREVIEW_CONTENTFUL_ENVIRONMENT = process.env.PUBLIC_CONTENTFUL_ENVIRONMENT;
--- src/models/WebsiteModel.ts
+++ src/models/WebsiteModel.ts
@@ -1,20 +1,34 @@
 import { BaseContentfulAPIModel, BaseController } from "scu-ssg";
 import { ContentTypeId } from "../types";
 import { IWebsite } from "../@types/generated/contentful";
-import { SCOTT_ROUSE_DEV_TAG, SCOTT_ROUSE_DEV_WEBSITE, SCOTT_ROUSE_OG_WEBSITE } from "../constants";
+import {
+  SCOTT_ROUSE_DESIGN_WEBSITE,
+  SCOTT_ROUSE_DEV_TAG,
+  SCOTT_ROUSE_DEV_WEBSITE,
+  SCOTT_ROUSE_OG_WEBSITE,
+  SCOTT_ROUSE_SYSTEM_WEBSITE
+} from "../constants";
 
 export default class WebsiteModel extends BaseContentfulAPIModel {
   contentTypeId = ContentTypeId.website;
 
   requiredEntities = [
     SCOTT_ROUSE_OG_WEBSITE,
-    SCOTT_ROUSE_DEV_WEBSITE
+    SCOTT_ROUSE_DEV_WEBSITE,
+    SCOTT_ROUSE_DESIGN_WEBSITE,
+    SCOTT_ROUSE_SYSTEM_WEBSITE
   ];
 }
 
 export function getWebsite(controller: BaseController): IWebsite {
   if (process.env.PUBLIC_CONTENTFUL_WEBSITE_TAG === SCOTT_ROUSE_DEV_TAG) {
     return controller.state.getEntry(SCOTT_ROUSE_DEV_WEBSITE) as unknown as IWebsite; 
+  }
+  if (process.env.PUBLIC_CONTENTFUL_WEBSITE_TAG === SCOTT_ROUSE_DESIGN_WEBSITE) {
+    return controller.state.getEntry(SCOTT_ROUSE_DESIGN_WEBSITE) as unknown as IWebsite; 
+  }
+  if (process.env.PUBLIC_CONTENTFUL_WEBSITE_TAG === SCOTT_ROUSE_SYSTEM_WEBSITE) {
+    return controller.state.getEntry(SCOTT_ROUSE_SYSTEM_WEBSITE) as unknown as IWebsite; 
   }
   return controller.state.getEntry(SCOTT_ROUSE_OG_WEBSITE) as unknown as IWebsite;
 }
